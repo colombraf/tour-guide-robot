0f044376a5e3b1a6018cff0b0bbc17e003d401d5
Raffaele Colombo <colombraf@gmail.com>
2023-11-28 11:10:39 +0100

    added -2

---
+++ app/navigation2/scripts/start_sim_dummy_r1cub-2.sh
@@ -0,0 +1,5 @@
+yarpserver &
+sleep 1;
+ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=/home/user1/tour-guide-robot/app/maps/SIM_GAM/dummy_r1cub-2.world &
+sleep 1;
+yarpmotorgui --robot r1cubSim &
